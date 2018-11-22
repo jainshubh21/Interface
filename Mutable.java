3c3a8b2d15f32823e9f61c369c3af4104113ae5f
Shubham jain <jainshubh21@gmail.com>
2018-11-23 00:13:48 +0530

    Interface Program

---
+++ Mutable.java
@@ -0,0 +1,19 @@
+
+public class Mutable 
+{
+	public static void main(String arg)
+	{
+	StringBuffer str = new StringBuffer("Shubham");
+	System.out.println("String is : "+str);
+	str.append("Jain");
+	System.out.println("String now is : "+str);
+	
+	StringBuilder str1 = new StringBuilder("Harish");
+	System.out.println("String is : "+str1);
+	str1.append("Kumar");
+	System.out.println("String now is : "+str1);
+	}
+	
+
+}
+ 	
